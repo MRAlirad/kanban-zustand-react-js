3727dd99b51ff3b7fd3c9062be7f091c1f41e56c
MRAlirad <m.r.alirad75@gmail.com>
2024-01-22 21:26:58 +0330

    persist

--- src/store.js
+++ src/store.js
@@ -1,14 +1,15 @@
 import {create} from "zustand";
+import {persist} from 'zustand/middleware';
 
 const store = (set) => ({
-    tasks: [{title: 'TestTask', state: 'PLANNED'}],
+    tasks: [],
     draggedTask: null,
     addTask: (title, state) => set((store) => ({tasks: [...store.tasks, {title, state}]})),
     deleteTask: (title) => set((store) => ({tasks: store.tasks.filter(task => task.title !== title)})),
     setDraggedTask: (title) => set({draggedTask: title}),
-    moveTask: (title, state)=> set(store => ({
-        tasks: store.tasks.map(task => task.title === title? {title, state} : task)
+    moveTask: (title, state) => set(store => ({
+        tasks: store.tasks.map(task => task.title === title ? {title, state} : task)
     }))
 });
 
-export const useStore = create(store);
+export const useStore = create(persist(store, { name: 'store' }));
